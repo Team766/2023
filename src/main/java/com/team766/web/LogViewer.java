e2c034f9d897e028c423be6648c5231e2b155fab
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-09-15 00:16:39 +0000

    Add auto-refresh to web LogViewer

--- src/main/java/com/team766/web/LogViewer.java
+++ src/main/java/com/team766/web/LogViewer.java
@@ -18,16 +18,34 @@ public class LogViewer implements WebServer.Handler {
 		
 		Logger logger = Logger.get(category);
 		
-		String r = "<h1>Log: " + category.toString() + "</h1>";
+        String r = "<h1>Log: " + category.toString() + "</h1>\n";
 		r += "<form><p>";
 		r += HtmlElements.buildDropDown("category", category.name(), Arrays.stream(Category.values()).map(Category::name).toArray(String[]::new));
 		r += "<input type=\"submit\" value=\"View\">";
 		r += "</p></form>\n";
-		r += "<table border=\"1\">\n";
+        r += "<table border=\"1\">\n";
 		for (RawLogEntry entry : logger.recentEntries()) {
-			r += String.format("<tr><td>%s</td><td>%s</td><td>%s</td><td style=\"white-space: pre\">%s</td></tr>", entry.getCategory(), entry.getTime(), entry.getSeverity(), entry.format());
+			r += String.format(
+                "<tr><td style=\"white-space: pre\">%s</td><td style=\"white-space: pre\">%s</td><td style=\"white-space: pre\">%s</td><td style=\"white-space: pre\">%s</td></tr>\n",
+                entry.getCategory(), entry.getTime(), entry.getSeverity(), entry.format());
 		}
-		r += "</table>\n";
+        r += "</table>\n";
+        r += "<input type=\"button\" onclick=\"window.location.reload();\" value=\"Refresh\" />\n";
+        r += "<input type=\"checkbox\" id=\"refresh-enabled\" checked=\"checked\" />";
+        r += "<label for=\"refresh-enabled\">Enable automatic refresh</label>\n";
+        r += "<script>";
+        r += "  setInterval(function(){";
+        r += "    if (document.getElementById('refresh-enabled').checked) {";
+        r += "      location.reload();";
+        r += "    }";
+        r += "  }, 3000);";
+        r += "  document.addEventListener('DOMContentLoaded', function(event) {";
+        r += "    window.scrollBy(0, document.body.scrollHeight);";
+        r += "    setTimeout(function(){";
+        r += "      window.scrollBy(0, document.body.scrollHeight);";
+        r += "    }, 150);";
+        r += "  });";
+        r += "</script>";
 		
 		return r;
 	}
