839969b4a794d61d15d5bb1f0ce1fbb15463fde0
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-09-20 07:19:32 +0000

    Switch LogViewer auto-refresh to AJAX
    Prevents the log page from reloading to a 404 error page when the server
    goes offline during redeploys, breaking the auto-refresh. Also makes it
    so the auto-refresh doesn't interrupt selecting a new logging category
    from the dropdown.

--- src/main/java/com/team766/web/LogViewer.java
+++ src/main/java/com/team766/web/LogViewer.java
@@ -18,34 +18,54 @@ public class LogViewer implements WebServer.Handler {
 		
 		Logger logger = Logger.get(category);
 		
-        String r = "<h1>Log: " + category.toString() + "</h1>\n";
-		r += "<form><p>";
-		r += HtmlElements.buildDropDown("category", category.name(), Arrays.stream(Category.values()).map(Category::name).toArray(String[]::new));
-		r += "<input type=\"submit\" value=\"View\">";
-		r += "</p></form>\n";
-        r += "<table border=\"1\">\n";
+        String r = String.join("\n", new String[]{
+            "<h1>Log: " + category.toString() + "</h1>",
+		    "<form><p>",
+		    HtmlElements.buildDropDown("category", category.name(), Arrays.stream(Category.values()).map(Category::name).toArray(String[]::new)),
+		    "<input type=\"submit\" value=\"View\">",
+		    "</p></form>",
+            "<table id=\"log-entries\" border=\"1\">"
+        });
 		for (RawLogEntry entry : logger.recentEntries()) {
 			r += String.format(
                 "<tr><td style=\"white-space: pre\">%s</td><td style=\"white-space: pre\">%s</td><td style=\"white-space: pre\">%s</td><td style=\"white-space: pre\">%s</td></tr>\n",
                 entry.getCategory(), entry.getTime(), entry.getSeverity(), entry.format());
 		}
-        r += "</table>\n";
-        r += "<input type=\"button\" onclick=\"window.location.reload();\" value=\"Refresh\" />\n";
-        r += "<input type=\"checkbox\" id=\"refresh-enabled\" checked=\"checked\" />";
-        r += "<label for=\"refresh-enabled\">Enable automatic refresh</label>\n";
-        r += "<script>";
-        r += "  setInterval(function(){";
-        r += "    if (document.getElementById('refresh-enabled').checked) {";
-        r += "      location.reload();";
-        r += "    }";
-        r += "  }, 3000);";
-        r += "  document.addEventListener('DOMContentLoaded', function(event) {";
-        r += "    window.scrollBy(0, document.body.scrollHeight);";
-        r += "    setTimeout(function(){";
-        r += "      window.scrollBy(0, document.body.scrollHeight);";
-        r += "    }, 150);";
-        r += "  });";
-        r += "</script>";
+        r += String.join("\n", new String[]{
+            "</table>",
+            "<input type=\"button\" onclick=\"window.location.reload();\" value=\"Refresh\" />",
+            "<input type=\"checkbox\" id=\"refresh-enabled\" checked=\"checked\" />",
+            "<label for=\"refresh-enabled\">Enable automatic refresh</label>",
+            "<script>",
+            "  function afterLoad(event) {",
+            "    window.scrollBy(0, document.body.scrollHeight);",
+            "    setTimeout(function(){",
+            "      window.scrollBy(0, document.body.scrollHeight);",
+            "    }, 150);",
+            "  }",
+            "  document.addEventListener('DOMContentLoaded', afterLoad);",
+            "  function refresh() {",
+            "    var xhttp = new XMLHttpRequest();",
+            "    xhttp.onreadystatechange = function() {",
+            "      if (this.readyState == 4 && this.status == 200) {",
+            "        var newDoc = new DOMParser().parseFromString(this.responseText, 'text/html')",
+            "        var oldTable = document.getElementById('log-entries');",
+            "        oldTable.parentNode.replaceChild(",
+            "            document.importNode(newDoc.querySelector('#log-entries'), true),",
+            "            oldTable);",
+            "        afterLoad();",
+            "      }",
+            "    };",
+            "    xhttp.open('GET', window.location.href, true);",
+            "    xhttp.send();",
+            "  }",
+            "  setInterval(function(){",
+            "    if (document.getElementById('refresh-enabled').checked) {",
+            "      refresh();",
+            "    }",
+            "  }, 3000);",
+            "</script>",
+        });
 		
 		return r;
 	}
