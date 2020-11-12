e3d8a288eec46df0351dc45c323405d121ccd136
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-11-12 03:57:22 +0000

    Add DoNothing default procedure

---
+++ src/main/java/com/team766/frc2020/procedures/DoNothing.java
@@ -0,0 +1,11 @@
+package com.team766.frc2020.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+
+public class DoNothing extends Procedure {
+
+	public void run(Context context) {
+	}
+	
+}
