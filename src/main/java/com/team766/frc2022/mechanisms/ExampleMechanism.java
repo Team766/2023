1640c7961919d1ac921ee8310d9a6e9332a0605c
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2022-02-08 23:25:16 -0800

    Add ExampleMechanism to make CI happy

---
+++ src/main/java/com/team766/frc2022/mechanisms/ExampleMechanism.java
@@ -0,0 +1,22 @@
+package com.team766.frc2022.mechanisms;
+
+import com.team766.framework.Mechanism;
+import com.team766.hal.RobotProvider;
+import com.team766.hal.SpeedController;
+
+public class ExampleMechanism extends Mechanism {
+	private SpeedController m_leftMotor;
+	private SpeedController m_rightMotor;
+
+	public ExampleMechanism() {
+		m_leftMotor = RobotProvider.instance.getTalonCANMotor("exampleMechanism.leftMotor");
+		m_rightMotor = RobotProvider.instance.getVictorCANMotor("exampleMechanism.rightMotor");
+	}
+
+	public void setMotorPower(double leftPower, double rightPower){
+		checkContextOwnership();
+
+		m_leftMotor.set(leftPower);
+		m_rightMotor.set(rightPower);
+	}
+}
