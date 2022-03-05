42ef3c6a0e0832bec673919f6a035e6845fa0204
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2022-03-05 09:02:02 -0800

    Better handling of ADXRS450 disconnected error

--- src/main/java/com/team766/hal/wpilib/ADXRS450_Gyro.java
+++ src/main/java/com/team766/hal/wpilib/ADXRS450_Gyro.java
@@ -1,17 +1,18 @@
 package com.team766.hal.wpilib;
 
 import com.team766.hal.GyroReader;
-
+import com.team766.logging.Category;
+import com.team766.logging.Logger;
+import com.team766.logging.Severity;
 import edu.wpi.first.wpilibj.SPI;
 
 public class ADXRS450_Gyro extends edu.wpi.first.wpilibj.ADXRS450_Gyro implements GyroReader {
 	public ADXRS450_Gyro(SPI.Port port) {
 		super(port);
 		if (!isConnected()) {
-			throw new RuntimeException("Gyro is not connected");
-		//	System.out.println("Gyro is not connected!");
+			Logger.get(Category.HAL).logData(Severity.ERROR, "ADXRS450 Gyro is not connected!");
 		} else {
-			System.out.println("Gyro is connected!");
+			Logger.get(Category.HAL).logData(Severity.INFO, "ADXRS450 Gyro is connected");
 		}
 	}
 
