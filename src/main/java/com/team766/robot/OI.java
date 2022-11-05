87cec902fa84fbd0215b04c05a98aeabbf5f5b7c
dgcodez <dg@voiceme.net>
2022-11-05 12:36:12 -0700

    initial bringup and basic code for Burrobot Prime
    Drive mechanism to control motors.
    OI to read from Logitech Gamepad (D mode) and control Drive.

--- src/main/java/com/team766/robot/OI.java
+++ src/main/java/com/team766/robot/OI.java
@@ -12,24 +12,33 @@ import com.team766.robot.procedures.*;
  * interface to the code that allow control of the robot.
  */
 public class OI extends Procedure {
-	private JoystickReader joystick0;
-	private JoystickReader joystick1;
-	private JoystickReader joystick2;
+	private final static int LEFT_STICK_X_AXIS = 0;
+	private final static int LEFT_STICK_Y_AXIS = 1;
+	private final static int RIGHT_STICK_X_AXIS = 2;
+	private final static int RIGHT_STICK_Y_AXIS = 3;
+
+	private final JoystickReader gamepad;
 	
 	public OI() {
 		loggerCategory = Category.OPERATOR_INTERFACE;
 
-		joystick0 = RobotProvider.instance.getJoystick(0);
-		joystick1 = RobotProvider.instance.getJoystick(1);
-		joystick2 = RobotProvider.instance.getJoystick(2);
+		gamepad = RobotProvider.instance.getJoystick(0);
 	}
 	
 	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+
 		while (true) {
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
-			
+			double leftX = gamepad.getAxis(LEFT_STICK_X_AXIS);
+			double leftY = gamepad.getAxis(LEFT_STICK_Y_AXIS);
+			double rightX = gamepad.getAxis(RIGHT_STICK_X_AXIS);
+			double rightY = gamepad.getAxis(RIGHT_STICK_Y_AXIS);
 
+			log(/*"leftX: " + leftX + */ ", leftY: " + leftY + ", rightX: " + rightX /*+ ", rightY: " + rightY */);
+
+			Robot.drive.setArcadeDrivePower(-leftY, rightX);
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}
 	}
