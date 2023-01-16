745b610ae0a2c4096dff53a60c18bcd10b4c33a3
dejabot <dg@dejabotz.com>
2023-01-15 20:03:50 -0800

    Fix HTML formatting in /config

--- src/main/java/com/team766/web/ConfigUI.java
+++ src/main/java/com/team766/web/ConfigUI.java
@@ -14,9 +14,10 @@ public class ConfigUI implements WebServer.Handler {
 	@Override
 	public String handle(Map<String, Object> params) {
 		String r = "<h1>Configuration (READ ONLY)</h1>";
+		r += "<p>";
 		r += "To edit, copy contents and place in <code>src/main/deploy/configs</code> directory.<br/>";
 		r += "See <a href=\"https://docs.google.com/document/d/1eKOG5iWi2tijRls1L9qZ4xBZx50y1EriIk2QBUTv86g/edit#\">docs</a> for more information.<br/>";
-		r += "<p>";
+		r += "</p>";
 		
 		// TODO: handle empty config file case.
 		r += "<textarea name=\"configJson\" readonly disabled style=\"background: lightgray; box-sizing: border-box; width: 100%; height: 400px;\">";
