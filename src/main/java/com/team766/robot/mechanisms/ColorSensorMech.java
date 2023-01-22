f6ee4ca383dfb9262db3ae6e2d5ad1f66cf669b2
Jayen Agrawal <119551497+HenrySeeger12@users.noreply.github.com>
2023-01-21 16:32:04 -0800

    non-functional rgb reader

---
+++ src/main/java/com/team766/robot/mechanisms/ColorSensorMech.java
@@ -0,0 +1,32 @@
+package com.team766.robot.mechanisms;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+import com.team766.framework.Mechanism;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+
+
+public class ColorSensorMech extends Mechanism {
+	private final I2C.Port i2cPort = /*???*/;
+	private final ColorSensorV3 m_colorSensor = new ColorSensorV3(/*i2cPort*/);
+	private final ColorMatch m_colorMatch = new ColorMatch();
+	//need to find rgb with sensor to get rgb vals
+	private final Color coneYellow = new Color(/*r, g, b*/);
+  	private final Color cubePurple = new Color(/*r, g, b*/);
+	//code just to grab rgb vals once
+	public void rgbFinder(){
+		int lastColor[]= {m_colorSensor.getRed(), m_colorSensor.getGreen(), m_colorSensor.getBlue()};
+			
+				//code only logs if the sensor is getting a new color so it doesn't log a million times
+				if((m_colorSensor.getRed()!=lastColor[0])||(m_colorSensor.getGreen()!=lastColor[1])||(m_colorSensor.getBlue()!=lastColor[2])){
+					log("red: "+m_colorSensor.getRed()+" green: "+m_colorSensor.getGreen()+" blue: "+m_colorSensor.getBlue());
+				}
+				lastColor[0]=m_colorSensor.getRed();
+				lastColor[1]=m_colorSensor.getGreen();
+				lastColor[2]=m_colorSensor.getBlue();
+		
+	}
+	
+}
