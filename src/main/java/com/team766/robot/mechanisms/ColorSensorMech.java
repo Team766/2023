83aa24a42d5702b029c7c3e72cf2cc457640aa00
Lydia Honerkamp <90945906+1yd1a@users.noreply.github.com>
2023-01-24 12:23:01 -0800

    possible solution 2 simplify rgb vals

--- src/main/java/com/team766/robot/mechanisms/ColorSensorMech.java
+++ src/main/java/com/team766/robot/mechanisms/ColorSensorMech.java
@@ -15,20 +15,20 @@ public class ColorSensorMech extends Mechanism {
 	}
 	//RGB MIGHT BE 16 BIT
 	public void senseRGB(){
-		int lastColor[]= {m_colorSensor.getRed(), m_colorSensor.getGreen(), m_colorSensor.getBlue()};
-				//code only logs if the sensor is getting a new color so it doesn't log a million times
-				log("in the method");
-				//if((m_colorSensor.getRed()!=lastColor[0])||(m_colorSensor.getGreen()!=lastColor[1])||(m_colorSensor.getBlue()!=lastColor[2])){
-					log("red: "+m_colorSensor.getRed()+" green: "+m_colorSensor.getGreen()+" blue: "+m_colorSensor.getBlue());
-				//}
-				lastColor[0]=m_colorSensor.getRed();
-				lastColor[1]=m_colorSensor.getGreen();
-				lastColor[2]=m_colorSensor.getBlue();
+		//proposed change: Color detectedColor = m_colorSensor.getColor();
+		int lastColor[]= {m_colorSensor.getRed(), m_colorSensor.getGreen(), m_colorSensor.getBlue()}
+		//proposed change: log("red: "+detectedColor.red+" green: "+detectedColor.green+" blue: "+detectedColor.blue);
+		//^have to see what the deal is with the dot notation colors --> just run it and check
+		log("red: "+m_colorSensor.getRed()+" green: "+m_colorSensor.getGreen()+" blue: "+m_colorSensor.getBlue());
+		//proposed change: omit				
+		lastColor[0]=m_colorSensor.getRed();
+		lastColor[1]=m_colorSensor.getGreen();
+		lastColor[2]=m_colorSensor.getBlue();
 	}
 	//similar thing but for getting proximity ranges
 	public void senseProx(){
 		int lastProx= m_colorSensor.getProximity();
-			
+				//proposed change: switch this to the every 5 seconds thing instead of the if
 				if(m_colorSensor.getProximity()!=lastProx){
 					log("proximity from base:"+m_colorSensor.getProximity());
 				}
