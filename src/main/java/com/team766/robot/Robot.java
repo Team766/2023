ee485a4a669ae5f6054e2e824f7b024c0ef76c7c
Jayen Agrawal <90945906+1yd1a@users.noreply.github.com>
2023-01-30 19:01:24 -0800

    starting intake branch...

--- src/main/java/com/team766/robot/Robot.java
+++ src/main/java/com/team766/robot/Robot.java
@@ -4,23 +4,10 @@ import com.team766.robot.mechanisms.*;
 
 public class Robot {
 	// Declare mechanisms here
-	public static Drive drive;
-	public static Gyro gyro;
-	public static CANdleMech candle;
-	public static Intake intake;
-	public static Storage storage;
-	public static Arms arms;
-	public static Grabber grabber;
+	public static Intake intake; 
 
 	public static void robotInit() {
 		// Initialize mechanisms here
-		drive = new Drive();
-		gyro = new Gyro();
-		candle = new CANdleMech();
 		intake = new Intake();
-		storage = new Storage();
-		arms = new Arms();
-		grabber = new Grabber();
-
 	}
 }
