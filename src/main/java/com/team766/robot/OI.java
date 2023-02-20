a9cb2ec17869639a74d37751f417c78f819d77ee
Alexandre Sauquet <54484828+SauquetAlex@users.noreply.github.com>
2023-02-20 10:53:12 -0800

    Small changes (mostly format)

--- src/main/java/com/team766/robot/OI.java
+++ src/main/java/com/team766/robot/OI.java
@@ -1,63 +1,71 @@
 package com.team766.robot;
 
-import com.team766.framework.Procedure;
 import com.team766.framework.Context;
+import com.team766.framework.Procedure;
 import com.team766.hal.JoystickReader;
 import com.team766.hal.RobotProvider;
-import com.team766.robot.mechanisms.*;
-
 import com.team766.logging.Category;
+import com.team766.robot.mechanisms.*;
 import com.team766.robot.procedures.*;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.math.geometry.Pose3d;
-
+import edu.wpi.first.wpilibj.DriverStation;
 
 /**
  * This class is the glue that binds the controls on the physical operator interface to the code
  * that allow control of the robot.
  */
 public class OI extends Procedure {
-	private JoystickReader joystick0;
-	private JoystickReader joystick1;
-	private JoystickReader joystick2;
 
-	public OI() {
-		loggerCategory = Category.OPERATOR_INTERFACE;
+  private JoystickReader joystick0;
+  private JoystickReader joystick1;
+  private JoystickReader joystick2;
 
-		joystick0 = RobotProvider.instance.getJoystick(0);
-		joystick1 = RobotProvider.instance.getJoystick(1);
-		joystick2 = RobotProvider.instance.getJoystick(2);
-		CameraServer.startAutomaticCapture();
-	}
+  public OI() {
+    loggerCategory = Category.OPERATOR_INTERFACE;
 
-	public void run(Context context) {
+    joystick0 = RobotProvider.instance.getJoystick(0);
+    joystick1 = RobotProvider.instance.getJoystick(1);
+    joystick2 = RobotProvider.instance.getJoystick(2);
+    CameraServer.startAutomaticCapture();
+  }
 
-		while (true) {
-			// wait for driver station data (and refresh it using the WPILib APIs)
-			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
-			RobotProvider.instance.refreshDriverStationData();
+  public void run(Context context) {
+    while (true) {
+      // wait for driver station data (and refresh it using the WPILib APIs)
+      context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
+      RobotProvider.instance.refreshDriverStationData();
 
-			// Add driver controls here - make sure to take/release ownership
-			// of mechanisms when appropriate.
-			context.takeOwnership(Robot.drive);
-			Robot.drive.setArcadeDrivePower(joystick0.getAxis(2), -1 * joystick0.getAxis(1));
-			context.releaseOwnership(Robot.drive);
+      // Add driver controls here - make sure to take/release ownership
+      // of mechanisms when appropriate.
+      context.takeOwnership(Robot.drive);
+      Robot.drive.setArcadeDrivePower(
+        joystick0.getAxis(2),
+        -1 * joystick0.getAxis(1)
+      );
+      context.releaseOwnership(Robot.drive);
 
-			// log("Is there a target? " + Robot.photonVision.hasTarget());
-			// log the x,y,z, and angle of the target
-			context.takeOwnership(Robot.photonVision);
-			try {
-				Pose3d pose = Robot.photonVision.getPose3d();
-				if (pose != null) {
-					log("X: " + pose.getX() + "\n Y: " + pose.getY() + "\n Z: " + pose.getZ());
-				} else {
-					// log("No pose");
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			context.releaseOwnership(Robot.photonVision);
-		}
-	}
+      // log("Is there a target? " + Robot.photonVision.hasTarget());
+      // log the x,y,z, and angle of the target
+      context.takeOwnership(Robot.photonVision);
+      try {
+        Pose3d pose = Robot.photonVision.getPose3d();
+        if (pose != null) {
+          log(
+            "X: " +
+            pose.getX() +
+            "\n Y: " +
+            pose.getY() +
+            "\n Z: " +
+            pose.getZ()
+          );
+        } else {
+          // log("No pose");
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      context.releaseOwnership(Robot.photonVision);
+    }
+  }
 }
--- src/main/java/com/team766/robot/mechanisms/PhotonVisionRevA.java
+++ src/main/java/com/team766/robot/mechanisms/PhotonVisionRevA.java
@@ -1,135 +1,183 @@
 package com.team766.robot.mechanisms;
 
-import java.io.IOException;
-import java.util.*;
-import org.photonvision.PhotonCamera;
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.PhotonPoseEstimator;
 // import org.photonvision.targeting.PhotonTrackedTarget;
 import com.team766.framework.Mechanism;
 import com.team766.logging.Category;
+import com.team766.robot.Constants.CameraConstants;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.Filesystem;
-
+import java.io.IOException;
+import java.util.*;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
 
 public class PhotonVisionRevA extends Mechanism {
-	// Set up cameras and various variables
-	PhotonCamera leftCamera;
-	PhotonCamera rightCamera;
 
-	Double leftCameraWeight;
-	Double rightCameraWeight;
+  // Set up cameras and various variables
+  PhotonCamera leftCamera;
+  PhotonCamera rightCamera;
 
-	AprilTagFieldLayout aprilTagFieldLayout;
+  Double leftCameraWeight;
+  Double rightCameraWeight;
 
-	Transform3d leftRobotToCam;
-	Transform3d rightRobotToCam;
+  AprilTagFieldLayout aprilTagFieldLayout;
 
-	PhotonPoseEstimator leftPhotonPoseEstimator;
-	PhotonPoseEstimator rightPhotonPoseEstimator;
+  Transform3d leftRobotToCam;
+  Transform3d rightRobotToCam;
 
-	/**
-	 * Constructor for PhotonVision.
-	 */
-	public PhotonVisionRevA() {
-		loggerCategory = Category.MECHANISMS;
+  PhotonPoseEstimator leftPhotonPoseEstimator;
+  PhotonPoseEstimator rightPhotonPoseEstimator;
 
-		// Initialize cameras. Make sure to use the same name you used in PhotonVision.
-		leftCamera = new PhotonCamera(CameraConstants.FRONT_LEFT_CAMERA_NAME);
-		rightCamera = new PhotonCamera(CameraConstants.FRONT_RIGHT_CAMERA_NAME);
+  /**
+   * Constructor for PhotonVision.
+   */
+  public PhotonVisionRevA() {
+    loggerCategory = Category.MECHANISMS;
 
-		// TODO: Set camera weights
-		leftCameraWeight = CameraConstants.FRONT_LEFT_CAMERA_WEIGHT;
-		rightCameraWeight = CameraConstants.FRONT_RIGHT_CAMERA_WEIGHT;
+    // Initialize cameras. Make sure to use the same name you used in PhotonVision.
+    leftCamera = new PhotonCamera(CameraConstants.FRONT_LEFT_CAMERA_NAME);
+    rightCamera = new PhotonCamera(CameraConstants.FRONT_RIGHT_CAMERA_NAME);
 
-		// Initialize field layout.
-		try {
-			aprilTagFieldLayout = new AprilTagFieldLayout(
-					Filesystem.getDeployDirectory().toPath().resolve(CameraConstants.FIELD_LAYOUT_FILE));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		// TODO test if rotation is correct (from robot to camera or camera to robot)
-		/*leftRobotToCam = new Transform3d(new Translation3d(0.022, 0.358, 0.838),
+    // TODO: Set camera weights
+    leftCameraWeight = CameraConstants.FRONT_LEFT_CAMERA_WEIGHT;
+    rightCameraWeight = CameraConstants.FRONT_RIGHT_CAMERA_WEIGHT;
+
+    // Initialize field layout.
+    try {
+      aprilTagFieldLayout =
+        new AprilTagFieldLayout(
+          Filesystem
+            .getDeployDirectory()
+            .toPath()
+            .resolve(CameraConstants.FIELD_LAYOUT_FILE)
+        );
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    // TODO test if rotation is correct (from robot to camera or camera to robot)
+    /*leftRobotToCam = new Transform3d(new Translation3d(0.022, 0.358, 0.838),
 				new Rotation3d(0, -15, 135));
 		rightRobotToCam = new Transform3d(new Translation3d(0.022, -0.358, 0.838),
 				new Rotation3d(0, -15, -135));
 		*/
-		leftRobotToCam = new Transform3d(new Translation3d(CameraConstants.FRONT_LEFT_CAMERA_OFFSET_X, CameraConstants.FRONT_LEFT_CAMERA_OFFSET_Y, CameraConstants.FRONT_LEFT_CAMERA_OFFSET_Z),
-				new Rotation3d(CameraConstants.FRONT_LEFT_CAMERA_OFFSET_PITCH, CameraConstants.FRONT_LEFT_CAMERA_OFFSET_YAW, CameraConstants.FRONT_LEFT_CAMERA_OFFSET_ROLL));
-		rightRobotToCam = new Transform3d(new Translation3d(CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_X, CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_Y, CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_Z),
-				new Rotation3d(CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_PITCH, CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_YAW, CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_ROLL));
-		// Initialize pose estimators.
-		leftPhotonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,
-				PhotonPoseEstimator.PoseStrategy.AVERAGE_BEST_TARGETS, leftCamera, leftRobotToCam);
-		rightPhotonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,
-				PhotonPoseEstimator.PoseStrategy.AVERAGE_BEST_TARGETS, rightCamera,
-				rightRobotToCam);
-	}
+    leftRobotToCam =
+      new Transform3d(
+        new Translation3d(
+          CameraConstants.FRONT_LEFT_CAMERA_OFFSET_X,
+          CameraConstants.FRONT_LEFT_CAMERA_OFFSET_Y,
+          CameraConstants.FRONT_LEFT_CAMERA_OFFSET_Z
+        ),
+        new Rotation3d(
+          CameraConstants.FRONT_LEFT_CAMERA_OFFSET_PITCH,
+          CameraConstants.FRONT_LEFT_CAMERA_OFFSET_YAW,
+          CameraConstants.FRONT_LEFT_CAMERA_OFFSET_ROLL
+        )
+      );
+    rightRobotToCam =
+      new Transform3d(
+        new Translation3d(
+          CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_X,
+          CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_Y,
+          CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_Z
+        ),
+        new Rotation3d(
+          CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_PITCH,
+          CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_YAW,
+          CameraConstants.FRONT_RIGHT_CAMERA_OFFSET_ROLL
+        )
+      );
+    // Initialize pose estimators.
+    leftPhotonPoseEstimator =
+      new PhotonPoseEstimator(
+        aprilTagFieldLayout,
+        PhotonPoseEstimator.PoseStrategy.AVERAGE_BEST_TARGETS,
+        leftCamera,
+        leftRobotToCam
+      );
+    rightPhotonPoseEstimator =
+      new PhotonPoseEstimator(
+        aprilTagFieldLayout,
+        PhotonPoseEstimator.PoseStrategy.AVERAGE_BEST_TARGETS,
+        rightCamera,
+        rightRobotToCam
+      );
+  }
 
-	/**
-	 * Pose estimator that returns an EstimatedRobotPose object.
-	 * 
-	 * @param photonPoseEstimator The PhotonPoseEstimator object to use.
-	 * @return An EstimatedRobotPose object, which can be null or an estimated pose,
-	 */
-	public Optional<EstimatedRobotPose> poseEstimate(PhotonPoseEstimator photonPoseEstimator) {
-		return photonPoseEstimator.update();
-	}
+  /**
+   * Pose estimator that returns an EstimatedRobotPose object.
+   *
+   * @param photonPoseEstimator The PhotonPoseEstimator object to use.
+   * @return An EstimatedRobotPose object, which can be null or an estimated pose,
+   */
+  public Optional<EstimatedRobotPose> poseEstimate(
+    PhotonPoseEstimator photonPoseEstimator
+  ) {
+    return photonPoseEstimator.update();
+  }
 
-	/**
-	 * Get the pose of the robot if possible.
-	 * 
-	 * @return The pose of the robot.
-	 */
-	public Pose3d getPose3d() {
-		Optional<EstimatedRobotPose> leftEstimate;
-		Optional<EstimatedRobotPose> rightEstimate;
-		leftEstimate = poseEstimate(leftPhotonPoseEstimator);
-		rightEstimate = poseEstimate(rightPhotonPoseEstimator);
+  /**
+   * Get the pose of the robot if possible.
+   *
+   * @return The pose of the robot.
+   */
+  public Pose3d getPose3d() {
+    Optional<EstimatedRobotPose> leftEstimate;
+    Optional<EstimatedRobotPose> rightEstimate;
+    leftEstimate = poseEstimate(leftPhotonPoseEstimator);
+    rightEstimate = poseEstimate(rightPhotonPoseEstimator);
 
-		if (leftEstimate == null || leftEstimate.isEmpty()) {
-			if (rightEstimate == null || rightEstimate.isEmpty()) {
-				return null;
-			} else {
-				return rightEstimate.get().estimatedPose;
-			}
-		} else {
-			if (rightEstimate == null || rightEstimate.isEmpty()) {
-				return leftEstimate.get().estimatedPose;
-			}
-		}
-		HashMap<Pose3d, Double> poses = new HashMap<Pose3d, Double>();
-		poses.put(leftEstimate.get().estimatedPose, leftCameraWeight);
-		poses.put(rightEstimate.get().estimatedPose, rightCameraWeight);
-		return averagPose3d(poses);
-	}
+    if (leftEstimate == null || leftEstimate.isEmpty()) {
+      if (rightEstimate == null || rightEstimate.isEmpty()) {
+        return null;
+      } else {
+        return rightEstimate.get().estimatedPose;
+      }
+    } else {
+      if (rightEstimate == null || rightEstimate.isEmpty()) {
+        return leftEstimate.get().estimatedPose;
+      }
+    }
+    HashMap<Pose3d, Double> poses = new HashMap<Pose3d, Double>();
+    poses.put(leftEstimate.get().estimatedPose, leftCameraWeight);
+    poses.put(rightEstimate.get().estimatedPose, rightCameraWeight);
+    return averagPose3d(poses);
+  }
 
-	/**
-	 * Averages a HashMap of Pose3d objects.
-	 * 
-	 * @param poses A HashMap of Pose3d objects and their weights.
-	 * @return The average Pose3d object or null if there are no weights.
-	 */
-	public Pose3d averagPose3d(HashMap<Pose3d, Double> poses) {
-		int numPoses = poses.size();
-		double weights = 0;
-		Pose3d averagePose = new Pose3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, 0.0));
-		for (HashMap.Entry<Pose3d, Double> entry : poses.entrySet()) {
-			Pose3d pose = entry.getKey();
-			Double weight = entry.getValue();
-			averagePose = averagePose.plus(new Transform3d(
-					new Pose3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, 0.0)), pose.times(weight)));
-			weights += weight.doubleValue();
-		}
-		if (weights != 0) {
-			return averagePose.times(1.0 / weights);
-		}
-		return null;
-	}
-
+  /**
+   * Averages a HashMap of Pose3d objects.
+   *
+   * @param poses A HashMap of Pose3d objects and their weights.
+   * @return The average Pose3d object or null if there are no weights.
+   */
+  public Pose3d averagPose3d(HashMap<Pose3d, Double> poses) {
+    int numPoses = poses.size();
+    double weights = 0;
+    Pose3d averagePose = new Pose3d(
+      0.0,
+      0.0,
+      0.0,
+      new Rotation3d(0.0, 0.0, 0.0)
+    );
+    for (HashMap.Entry<Pose3d, Double> entry : poses.entrySet()) {
+      Pose3d pose = entry.getKey();
+      Double weight = entry.getValue();
+      averagePose =
+        averagePose.plus(
+          new Transform3d(
+            new Pose3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, 0.0)),
+            pose.times(weight)
+          )
+        );
+      weights += weight.doubleValue();
+    }
+    if (weights != 0) {
+      return averagePose.times(1.0 / weights);
+    }
+    return null;
+  }
 }
