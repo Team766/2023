4cc79a032bc81c6fafc466c0dba239e625bb2beb
Adrian Deutscher-Bishop <bcnofnenamg@gmail.com>
2023-02-20 12:05:24 -0800

    Added swerveOdometry Code

--- src/main/java/com/team766/robot/OI.java
+++ src/main/java/com/team766/robot/OI.java
@@ -70,45 +70,7 @@ public class OI extends Procedure {
 			} else {
 				RightJoystick_X = 0;	
 			}
-			if(Math.abs(joystick1.getAxis(InputConstants.AXIS_TWIST)) > 0.05){
-				RightJoystick_Theta = joystick1.getAxis(InputConstants.AXIS_TWIST);
-			} else {
-				RightJoystick_Theta = 0;
-			}
-			if(Math.abs(joystick0.getAxis(InputConstants.AXIS_FORWARD_BACKWARD)) > 0.05){
-				LeftJoystick_Y = joystick0.getAxis(InputConstants.AXIS_FORWARD_BACKWARD);
-			} else {
-				LeftJoystick_Y = 0;
-			}
-			if(Math.abs(joystick0.getAxis(InputConstants.AXIS_LEFT_RIGHT)) > 0.05){
-				LeftJoystick_X = joystick0.getAxis(InputConstants.AXIS_LEFT_RIGHT);
-			} else {
-				LeftJoystick_X = 0;
-			}
-			if(Math.abs(joystick0.getAxis(InputConstants.AXIS_TWIST)) > 0.05){
-				LeftJoystick_Theta = joystick0.getAxis(InputConstants.AXIS_TWIST);
-			} else {
-				LeftJoystick_Theta = 0;
-			}
-			//log(Robot.gyro.getGyroYaw());			
-			//TODO: fix defense: the robot basically locks up if there is defense
-			// if(joystick0.getButton(InputConstants.CROSS_DEFENSE)){
-			// 	context.startAsync(new DefenseCross());
-			// }
 			
-			/*if(Math.pow(Math.pow(joystick0.getAxis(InputConstants.AXIS_LEFT_RIGHT),2) + Math.pow(joystick0.getAxis(InputConstants.AXIS_FORWARD_BACKWARD),2), 0.5) > 0.15 ){
-				Robot.drive.drive2D(
-					((joystick0.getAxis(InputConstants.AXIS_LEFT_RIGHT))),
-					((joystick0.getAxis(InputConstants.AXIS_FORWARD_BACKWARD)))
-				);
-			}  else {
-				if(Math.abs(joystick0.getAxis(InputConstants.AXIS_TWIST))>=0.1){
-					Robot.drive.turning(joystick0.getAxis(InputConstants.AXIS_TWIST));
-				} else {
-				Robot.drive.stopDriveMotors();
-				Robot.drive.stopSteerMotors();
-				}
-			}*/
 			if(joystick0.getButtonPressed(1))
 				Robot.gyro.resetGyro();
 
@@ -117,19 +79,6 @@ public class OI extends Procedure {
 
 			if(joystick1.getButtonPressed(1))
 				isCross = !isCross;
-			
-			
-			if(joystick0.getButtonPressed(2)){
-				Robot.drive.setFrontRightEncoders();
-				Robot.drive.setFrontLeftEncoders();
-				Robot.drive.setBackRightEncoders();
-				Robot.drive.setBackLeftEncoders();
-			}
-			// if(joystick1.getButton(1)){
-			// 	turningValue = joystick1.getAxis(InputConstants.AXIS_LEFT_RIGHT); 
-			// } else {
-			// 	turningValue = 0;
-			// }
 			
 			if (isCross)  {
 				context.startAsync(new setCross());
