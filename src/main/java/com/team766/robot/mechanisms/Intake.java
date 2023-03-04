3e9972045462a98716ef7db389eefd1483107064
Lydia Honerkamp <hermionepusheen@gmail.com>
2023-03-03 20:14:21 -0800

    I FIXED 1 MILLION ERRORS (NOT CLICKBAIT)

--- src/main/java/com/team766/robot/mechanisms/Intake.java
+++ src/main/java/com/team766/robot/mechanisms/Intake.java
@@ -1,49 +1,94 @@
-package com.team766.robot.mechanisms;
+package com.team766.frc2022.mechanisms;
 
+import com.team766.config.ConfigFileReader;
 import com.team766.framework.Mechanism;
-import com.team766.hal.RobotProvider;
-import com.team766.hal.MotorController;
 import com.team766.hal.SolenoidController;
+import com.team766.hal.CANSpeedController;
+import com.team766.hal.RobotProvider;
+import com.team766.hal.wpilib.DigitalInput;
 
 public class Intake extends Mechanism {
-	private SolenoidController m_intakeArm1;
-	private SolenoidController m_intakeArm2;
-	private MotorController m_frontIntakeWheel;
-	private MotorController m_middleIntakeWheel;
-
-	//for now this is just a copy of the 2020 intake so config names are most likely wrong
+	private SolenoidController intakeArm1;
+	private SolenoidController intakeArm2;
+	private MotorController topIntakeWheels;
+	private MotorController bottomIntakeWheels;
+	private MotorController conveyorBelt;
+	private boolean beltGoing = false;
+	
 	public Intake() {
-		m_frontIntakeWheel = RobotProvider.instance.getMotor("intake.frontWheel");
-		m_middleIntakeWheel = RobotProvider.instance.getMotor("intake.topWheel");
-		m_intakeArm1 = RobotProvider.instance.getSolenoid("Intake.intakeArm1");
-		m_intakeArm2 = RobotProvider.instance.getSolenoid("Intake.intakeArm2");
-		m_frontIntakeWheel.setInverted(true);
+		topIntakeWheels = RobotProvider.instance.getMotor("Intake.topWheels");
+		bottomIntakeWheels = RobotProvider.instance.getMotor("Intake.bottomWheels");
+		conveyorBelt = RobotProvider.instance.getMotor("Intake.conveyorBelt");
+
+		intakeArm1 = RobotProvider.instance.getSolenoid("Intake.intakeArm1");
+		intakeArm2 = RobotProvider.instance.getSolenoid("Intake.intakeArm2");
+
 	}
 
-	public void IntakeIn() {
+	public void startIntake() {
 		checkContextOwnership();
+		double power = ConfigFileReader.getInstance().getDouble("Intake.intakePower").get();
+		
+		startArms();
+		topIntakeWheels.set(power);
+		bottomIntakeWheels.set(power);
+		conveyorBelt.set(power);
 
-		m_intakeArm1.set(true);
-		m_intakeArm2.set(false);
-		m_frontIntakeWheel.set(1.0);
-		m_middleIntakeWheel.set(1.0);
-	}
-
-	public void IntakeOut(){
-		checkContextOwnership();
-
-		m_intakeArm1.set(true);
-		m_intakeArm2.set(false);
-		m_frontIntakeWheel.set(-1.0);
-		m_middleIntakeWheel.set(-1.0);
 	}
 
 	public void stopIntake() {
 		checkContextOwnership();
+		
+		topIntakeWheels.set(0.0);
+		bottomIntakeWheels.set(0.0);
+		conveyorBelt.set(0.0);
+		stopArms();
+	}
+	/*
+	* Intake code that would use color sensor 
+	* to automatically stop intake when piece reaches end of belt.
+	* Can  be manually stopped by pressing button again.
+	*
+	* checkContextOwnership();
+	* double power = ConfigFileReader.getInstance().getDouble("Intake.intakePower").get();
+    *
+	* if(beltGoing){
+	*	topIntakeWheels.set(0.0);
+	*   bottomIntakeWheels.set(0.0);
+	*	conveyorBelt.set(0.0);
+	*	stopArms();
+	* } else {
+	*	startArms();
+	*	topIntakeWheels.set(power);
+	*	bottomIntakeWheels.set(power);
+	*	conveyorBelt.set(power);
+	* }
+	* a different method will check if the sensor sees stuff
+	* if(colorSensor.getStorage) stop the motors and arms
+	*/
 
-		m_middleIntakeWheel.set(0.0);
-		m_frontIntakeWheel.set(0.0);
-		m_intakeArm1.set(false);
-		m_intakeArm2.set(true);
+	public void startArms(){
+		checkContextOwnership();
+
+		intakeArm1.set(true);
+		intakeArm2.set(true);
+	}
+
+	public void stopArms(){
+		checkContextOwnership();
+
+		intakeArm1.set(false);
+		intakeArm2.set(false);
+	}
+
+	public void reverseIntake(){
+		checkContextOwnership();
+		double power = ConfigFileReader.getInstance().getDouble("Intake.intakePower").get();
+
+		topIntakeWheels.set(-power);
+		bottomIntakeWheels.set(-power);
+		conveyorBelt.set(-power);
+
+		startArms();
 	}
 }
