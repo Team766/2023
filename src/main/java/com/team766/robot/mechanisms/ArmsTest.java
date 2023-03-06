6331a4e2d1dfb7fb4ac67fb24e72ed792768ac08
TTVMixmix00 <68516760+TTVMixmix00@users.noreply.github.com>
2023-03-06 08:37:09 -0800

    ﻿ - Temporary commit.

---
+++ src/main/java/com/team766/robot/mechanisms/ArmsTest.java
@@ -0,0 +1,32 @@
+package com.team766.robot.mechanisms;
+
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.team766.controllers.PIDController;
+import com.team766.framework.Mechanism;
+import com.team766.hal.MotorController;
+import com.team766.hal.RobotProvider;
+import com.team766.hal.MotorController.ControlMode;
+import com.team766.hal.wpilib.CANSparkMaxMotorController;
+import com.team766.hal.EncoderReader;
+
+public class ArmsTest extends Arms{
+
+	public ArmsTest(){
+
+	}
+
+	public void test(){
+		firstJoint.set(0.05);
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
