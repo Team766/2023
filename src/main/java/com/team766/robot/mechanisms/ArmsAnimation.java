f4e045363240b4104fa5f577d7d9dcca41dd3cf3
James <yongkimleng@gmail.com>
2023-04-01 03:34:20 -0700

    no message

--- src/main/java/com/team766/robot/mechanisms/ArmsAnimation.java
+++ src/main/java/com/team766/robot/mechanisms/ArmsAnimation.java
@@ -1,8 +1,11 @@
+package com.team766.robot.mechanisms;
+
+import java.util.HashMap;
 
 public class ArmsAnimation {
 
 	public enum AnimationState {
-		DISABLED = 0,			// valid next states (depending on STOW or scoring): UNSTOWED or PREP
+		DISABLED,				// valid next states (depending on STOW or scoring): UNSTOWED or PREP
 		STOWED,					// valid next states: UNSTOWED
 		UNSTOWED,				// valid next states: STOWED, PREP
 		PREP,					// valid next states: UNSTOWED, PLAYER_STATION_CONE, MID_GOAL, HIGH_GOAL
@@ -33,17 +36,17 @@ public class ArmsAnimation {
 
 	private AnimationState currentAnimationState = AnimationState.DISABLED;
 
-	public final HashMap<AnimationState, Waypoint> waypoints = new HashMap<AnimationState, WaypointDefinition>();
+	public final HashMap<AnimationState, Waypoint> waypoints = new HashMap<AnimationState, Waypoint>();
 	
 	public ArmsAnimation() {
 		// TODO: these are just estimates, calibrate on before using otherwise something may break :D
-		waypoints.put(DISABLED, null);
-		waypoints.put(STOWED, 				new Waypoint(0, -150, BOTH_JOINTS_REACHED, 500 ));
-		waypoints.put(UNSTOWED, 			new Waypoint(10, -140, JOINT_ONE_REACHED_ONLY, 3000));
-		waypoints.put(PREP, 				new Waypoint(10, -90, BOTH_JOINTS_REACHED, 4000));
-		waypoints.put(PLAYER_STATION_CONE, 	new Waypoint(-20, -80, BOTH_JOINTS_REACHED, 3000));
-		waypoints.put(MID_GOAL, 			new Waypoint(-20, -110, BOTH_JOINTS_REACHED, 3000));
-		waypoints.put(HIGH_GOAL 			new Waypoint(-20, -50, BOTH_JOINTS_REACHED, 3000));
+		waypoints.put(AnimationState.DISABLED,				null);
+		waypoints.put(AnimationState.STOWED, 				new Waypoint(0, -150, ProceedCondition.BOTH_JOINTS_REACHED, 500 ));
+		waypoints.put(AnimationState.UNSTOWED, 				new Waypoint(10, -140,ProceedCondition.JOINT_ONE_REACHED_ONLY, 3000));
+		waypoints.put(AnimationState.PREP, 					new Waypoint(10, -90, ProceedCondition.BOTH_JOINTS_REACHED, 4000));
+		waypoints.put(AnimationState.PLAYER_STATION_CONE, 	new Waypoint(-20, -80, ProceedCondition.BOTH_JOINTS_REACHED, 3000));
+		waypoints.put(AnimationState.MID_GOAL, 				new Waypoint(-20, -110, ProceedCondition.BOTH_JOINTS_REACHED, 3000));
+		waypoints.put(AnimationState.HIGH_GOAL, 			new Waypoint(-20, -50, ProceedCondition. BOTH_JOINTS_REACHED, 3000));
 	}
 
 	public void update() {
