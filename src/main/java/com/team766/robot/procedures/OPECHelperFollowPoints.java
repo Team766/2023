ccc4f34f527f7fccacdd89bce994b681cc46c52f
Adrian Deutscher-Bishop <bcnofnenamg@gmail.com>
2023-04-07 12:11:19 -0700

    Added FollowPoints auton methods

---
+++ src/main/java/com/team766/robot/procedures/OPECHelperFollowPoints.java
@@ -0,0 +1,35 @@
+package com.team766.robot.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.robot.Robot;
+import com.team766.robot.constants.FollowPointsInputConstants;
+import java.util.function.BooleanSupplier;
+import edu.wpi.first.wpilibj.DriverStation;
+import com.team766.odometry.*;
+
+public class OPECHelperFollowPoints extends Procedure {
+
+	private static final double DIST = 4;
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+		context.takeOwnership(Robot.intake);
+		Robot.gyro.resetGyro180();
+		Robot.drive.setGyro(Robot.gyro.getGyroYaw());
+		new ReverseIntake().run(context);
+		context.releaseOwnership(Robot.drive);
+		switch (DriverStation.getAlliance()) {
+			case Blue:
+				new FollowPoints(new PointDir[]{new PointDir(4, 0)}).run(context);
+				break;
+			case Red:
+				new FollowPoints(new PointDir[]{new PointDir(-4, 0)}).run(context);
+				break;
+			case Invalid: //drop down
+			default: 
+				log("invalid alliance");
+				return;
+		}
+	}
+}
