54198b5e8ffb33fe8fd3f9e0d6909cbe85669408
Max <mspier00001@gmail.com>
2023-05-29 21:09:22 -0700

    added a lot, changed some, just check the description
    (-) old photonvison code that was in their examples
    (/) followed their documentation through the photonvision website
    (+) added method to easily find the best target
    (+) added method to get yaw
    (+) added method to get pitch
    (+) added method to get area that the target fills the screen
    (+) added method to get the target ID of the April tag
    (+) added dead zones (dz's) and a way to set them
    (+) added a way to calculate position badly
    (+) added a go()

--- src/main/java/com/team766/robot/mechanisms/januaryTag.java
+++ src/main/java/com/team766/robot/mechanisms/januaryTag.java
@@ -14,37 +14,124 @@ import com.team766.hal.RobotProvider;
 import com.team766.library.RateLimiter;
 import com.team766.library.ValueProvider;
 //import com.team766.logging.Category;
-
+import de.erichseifert.gral.util.GeometryUtils;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class januaryTag extends Mechanism{
 	private PhotonCamera camera1;
-    private PhotonPoseEstimator photonPoseEstimator;
-
+    private double deadzoneX;
+    private double deadzoneY;
+    private double x_target;
+    private double y_target;
+    private MotorController leftMotor;
+    private MotorController rightMotor;
 	public januaryTag(){
 		camera1 = new PhotonCamera("januaryTag.camera1");
-
-		try {
-            // Attempt to load the AprilTagFieldLayout that will tell us where the tags are on the field.
-            AprilTagFieldLayout fieldLayout = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
-            // Create pose estimator
-            photonPoseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.MULTI_TAG_PNP, photonCamera, VisionConstants.robotToCam);
-            photonPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
-        } catch (IOException e) {
-            // The AprilTagFieldLayout failed to load. We won't be able to estimate poses if we don't know
-            // where the tags are.
-            log("Failed to load AprilTagFieldLayout");
-            photonPoseEstimator = null;
-        }
+        leftMotor = RobotProvider.instance.getMotor("leftMotor");
+        rightMotor = RobotProvider.instance.getMotor("rightMotor");
 	}
+
+    public PhotonTrackedTarget getBestTrackedTarget(){
+        var result = camera1.getLatestResult(); //getting the result from the camera
+        boolean hasTargets = result.hasTargets(); // checking to see if there are any targets in the camera's view. IF THERE ISN'T AND YOU USE result.getTargets() YOU WILL GET AN ERROR
+
+        if(hasTargets){
+            List<PhotonTrackedTarget> targets = result.getTargets(); // getting targets
+            PhotonPipelineResult photonPipelineResult = new PhotonPipelineResult(2, targets); // TODO: Replace latencyMillis with real latency time
+
+            PhotonTrackedTarget bestTrackedTarget = result.getBestTarget(); // getting the best target that is currently being picked up by the camera so that it can know where it is
+            return bestTrackedTarget;
+        }else{
+            log("No targets? see what i did there");
+            throw new januaryTagException("There were no targets that could be picked up by the camera, so I'm gonna have to throw this error here.");
+        }
+    }
+
+    public double getYaw(){
+        PhotonTrackedTarget theTarget = getBestTrackedTarget();
+        return theTarget.getYaw();
+    }
+
+    public double getPitch(){
+        PhotonTrackedTarget theTarget = getBestTrackedTarget();
+        return theTarget.getPitch();
+    }
+
+    public double getArea(){
+        PhotonTrackedTarget theTarget = getBestTrackedTarget();
+        return theTarget.getArea();
+    }
+
+    public int getTargetID(){
+        PhotonTrackedTarget theTarget = getBestTrackedTarget();
+        return theTarget.getFiducialId();
+    }
+    
+    public void setDeadzoneX(double dz){
+        deadzoneX = dz;
+    }
+
+    public void setDeadzoneY(double dz){
+        deadzoneY = dz;
+    }
+
+    public void setDeadzones(double x, double y){
+        deadzoneX = x;
+        deadzoneY = y;
+    }
+
+    public void findTargets(double x_scoring, double y_scoring){
+        double robotX_pos = getYaw();
+        double robotY_pos = getPitch();
+
+        double delta_x = x_scoring - robotX_pos;
+        x_target = x_scoring;
+        y_target = y_scoring - (0.2) * Math.abs(delta_x);
+    }
+
+    public void go(double x_scoring, double y_scoring){
+        while(2>1){
+            if(getYaw() + deadzoneX > x_target && getYaw() - deadzoneX < x_target && getPitch() + deadzoneY > y_target && getPitch() - deadzoneY < y_target){
+                log("Outtaking");
+                break;
+            }else{
+                double robotX_pos = getYaw();
+                double robotY_pos = getPitch();
+
+                double delta_x = x_scoring - robotX_pos;
+                double forward = x_scoring;
+                double turn = y_scoring - (0.2) * Math.abs(delta_x);
+
+                
+                double leftMotorPower = (0.2) * turn + (0.2) * forward;
+		        double rightMotorPower = (-0.2) * turn + (0.2) * forward;
+                
+                leftMotor.set(leftMotorPower);
+                rightMotor.set(rightMotorPower);
+            }
+        }
+    }
+
+
+
+    
+
+    public void debugLogs(){
+
+    }
 
 }
