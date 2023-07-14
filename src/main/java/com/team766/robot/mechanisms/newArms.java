2a01d27cd2e45da76526adbf0b62b101aa80e1dd
TTVMixmix00 <68516760+TTVMixmix00@users.noreply.github.com>
2023-07-14 14:39:37 +0200

    OI is so goofy
    Why cant we just create a fork at the beginning of the season???

--- src/main/java/com/team766/robot/mechanisms/newArms.java
+++ src/main/java/com/team766/robot/mechanisms/newArms.java
@@ -14,7 +14,7 @@ import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 import com.team766.library.RateLimiter;
 import com.team766.library.ValueProvider;
-import com.team766.logging.Category;
+//import com.team766.logging.Category;
 //This is for the motor that controls the pulley
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -23,20 +23,29 @@ public class newArms extends Mechanism {
 	CSMMCPID arm1;
 	CSMMCPID arm2;
 	public newArms(){
-		arm1 = new CSMMCPID("newArms.arm1");
+		try{
+			arm1 = new CSMMCPID("newArms.arm1");
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
 		arm1.setPIDF(0, 0, 0, 0);
 		arm1.setOutputRange(1, 1);
-		arm1.setMaxMinLocation(0, 0);
+		arm1.setMinMaxLocation(0, 0);
 		arm1.setDeadzone(2);
 		arm1.setMaxAccel(0);
 		arm1.setMaxVel(2);
 		arm1.setMotorMode(NeutralMode.Brake);
 		arm1.setAntigravConstant(0);
-
-		arm2 = new CSMMCPID("newArms.arm2");
+		try{
+			arm2 = new CSMMCPID("newArms.arm2");	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		arm2.setPIDF(0, 0, 0, 0);
 		arm2.setOutputRange(1, 1);
-		arm2.setMaxMinLocation(0, 0);
+		arm2.setMinMaxLocation(0, 0);
 		arm2.setDeadzone(2);
 		arm2.setMaxAccel(0);
 		arm2.setMaxVel(2);
@@ -46,14 +55,14 @@ public class newArms extends Mechanism {
 	}
 
 	public void go1(double position){
-		arm1.setAllusion(position);
+		arm1.setPosition(position);
 	}
 	public void go2(double position){
-		arm2.setAllusion(position);
+		arm2.setPosition(position);
 	}
 	public void goBoth(double position1, double position2){
-		arm1.setAllusion(position1);
-		arm2.setAllusion(position2);
+		arm1.setPosition(position1);
+		arm2.setPosition(position2);
 	}
 
 }
