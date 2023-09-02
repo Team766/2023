5915ab8e1ac9c38b03b0ba84a1d36af17e9cf121
Max <>
2023-09-02 14:42:48 -0700

    my code works the haters can all suck it

--- src/main/java/com/team766/robot/mechanisms/Kicker.java
+++ src/main/java/com/team766/robot/mechanisms/Kicker.java
@@ -3,15 +3,38 @@ package com.team766.robot.mechanisms;
 import com.team766.controllers.PIDController;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Kicker {
 	
 	private MotorController motor = RobotProvider.instance.getMotor("kickerMotor");
+	private CANSparkMax m_motor;
+	private static final SparkMaxAlternateEncoder.Type kAltEncType = SparkMaxAlternateEncoder.Type.kQuadrature;
+	private static final int kCPR = 8192;
+	private static final MotorType kMotorType = MotorType.kBrushless;
+
+/*
+   * An alternate encoder object is constructed using the GetAlternateEncoder() 
+   * method on an existing CANSparkMax object. If using a REV Through Bore 
+   * Encoder, the type should be set to quadrature and the counts per 
+   * revolution set to 8192
+*/
+  	
+
+	m_motor = new CANSparkMax(0, kMotorType);
+
+	private RelativeEncoder alternateEncoder = m_motor.getAlternateEncoder(kAltEncType, kCPR);
+
+
 	private PIDController pid = new PIDController(0.1, 0, 0, 0, -1.0,1.0,0.005); //TODO: CALIBRATE VALUES
 	private final double firstPoint = 0.000; //TODO: CHANGE
 	private final double secondPoint = 0.000; //TODO: CHANGE
 
 	private double curSetpoint;
+
 	public Kicker(){
 
 	}
@@ -36,7 +59,7 @@ public class Kicker {
 	 */
 	public void run(){
 		pid.setSetpoint(curSetpoint);
-		//pid.calculate( ABSOLUTE ENCODER POSITION);
+		pid.calculate(alternateEncoder.getPosition());
 		motor.set(pid.getOutput());
 	}
 
