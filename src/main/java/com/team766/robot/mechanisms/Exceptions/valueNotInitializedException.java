4ef99b132690abc06d1f7e12ef7cc6f31e4cfcc2
TTVMixmix00 <68516760+TTVMixmix00@users.noreply.github.com>
2023-10-02 17:08:52 +0000

    add new exception and heading on scoringareaapriltag

---
+++ src/main/java/com/team766/robot/mechanisms/Exceptions/valueNotInitializedException.java
@@ -0,0 +1,7 @@
+package com.team766.robot.mechanisms.Exceptions;
+
+public class valueNotInitializedException extends RuntimeException {
+	public valueNotInitializedException (String errorMessage) {
+		super(errorMessage);
+	} 
+}
--- src/main/java/com/team766/robot/mechanisms/ScoringAreaApriltag.java
+++ src/main/java/com/team766/robot/mechanisms/ScoringAreaApriltag.java
@@ -6,9 +6,17 @@ public class ScoringAreaApriltag {
 
 	private double x;
 	private double y;
+	private double heading = -1;
+
 	public ScoringAreaApriltag(Location l, int ID){
 		x = l.getX();
 		y = l.getY();
+	}
+
+	// override, will change it to only this constructor later but dont want to break everything.
+	public ScoringAreaApriltag(Location l, int ID, double heading){
+		this(l, ID);
+		this.heading = heading;
 	}
 
 	public double getX(){
@@ -21,5 +29,12 @@ public class ScoringAreaApriltag {
 
 	public int getTagID(){
 		return ID;
+	}
+
+	public double getHeading(){
+		if(heading < 0){
+			throw new valueNotInitializedException("Heading was not initialized for AprilTag with tag ID " + getTagID() + " at field X position " + getX() + " and field Y position " + getY());
+		}
+		return heading;
 	}
 }
