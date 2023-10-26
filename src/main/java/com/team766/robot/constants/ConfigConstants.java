37db4ba9abd86043c8cb215ff6984e84b00b53f5
qntmcube <rg@voiceme.net>
2023-10-25 18:48:33 -0700

    simple config naming changes for bringup

--- src/main/java/com/team766/robot/constants/ConfigConstants.java
+++ src/main/java/com/team766/robot/constants/ConfigConstants.java
@@ -21,21 +21,21 @@ public final class ConfigConstants {
 
 	// wrist config values
 	public static final String WRIST_MOTOR = "wrist.motor";
-	public static final String WRIST_PGAIN = "wrist.pGain";
-	public static final String WRIST_IGAIN = "wrist.iGain";
-	public static final String WRIST_DGAIN = "wrist.dGain";
-	public static final String WRIST_FFGAIN = "wrist.ffGain";
+	public static final String WRIST_PGAIN = "wrist.sparkPID.pGain";
+	public static final String WRIST_IGAIN = "wrist.sparkPID.iGain";
+	public static final String WRIST_DGAIN = "wrist.sparkPID.dGain";
+	public static final String WRIST_FFGAIN = "wrist.sparkPID.ffGain";
 
 	// elevator config values
 	public static final String ELEVATOR_LEFT_MOTOR = "elevator.leftMotor";
 	public static final String ELEVATOR_RIGHT_MOTOR = "elevator.rightMotor";
-	public static final String ELEVATOR_PGAIN = "elevator.pGain";
-	public static final String ELEVATOR_IGAIN = "elevator.iGain";
-	public static final String ELEVATOR_DGAIN = "elevator.dGain";
-	public static final String ELEVATOR_FFGAIN = "elevator.ffGain";
-	public static final String ELEVATOR_MAX_VELOCITY = "elevator.maxVelocity";
-	public static final String ELEVATOR_MIN_OUTPUT_VELOCITY = "elevator.minOutputVelocity";
-	public static final String ELEVATOR_MAX_ACCEL = "elevator.maxAccel";
+	public static final String ELEVATOR_PGAIN = "elevator.sparkPID.pGain";
+	public static final String ELEVATOR_IGAIN = "elevator.sparkPID.iGain";
+	public static final String ELEVATOR_DGAIN = "elevator.sparkPID.dGain";
+	public static final String ELEVATOR_FFGAIN = "elevator.sparkPID.ffGain";
+	public static final String ELEVATOR_MAX_VELOCITY = "elevator.sparkPID.maxVelocity";
+	public static final String ELEVATOR_MIN_OUTPUT_VELOCITY = "elevator.sparkPID.minOutputVelocity";
+	public static final String ELEVATOR_MAX_ACCEL = "elevator.sparkPID.maxAccel";
 }
 
 
